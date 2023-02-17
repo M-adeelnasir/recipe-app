e46bc6d2560c5e95cfbea24bed397d4a58e68062
unknown <adeelnasirkbw@gmail.com>
2023-02-18 01:40:08 +0500

    docker-compose and git cicd configured for wait_for _db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,6 @@ from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
 
-from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
