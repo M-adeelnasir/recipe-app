4ce80a0f36713ebb832452cb1c2a160e962f4133
unknown <adeelnasirkbw@gmail.com>
2023-02-18 03:52:07 +0500

    user and super user create tested

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,25 @@ class ModelTest(TestCase):
         )
         self.assertEqual(user.email,email)
         self.assertTrue(user.check_password(password))
+
+    def test_user_email_normalized(self):
+        sample_email = [
+            ['test1@EXAMPLE.com','test1@example.com'],
+            ['Test2@Example.com','Test2@example.com'],
+            ['test3@example.COM','test3@example.com']
+        ]
+        for email, expected in sample_email:
+            user = get_user_model().objects.create_user(email,'pass123')
+            self.assertEqual(user.email,expected)
+
+    def test_raise_value_error_on_not_providing_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','pass123')
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'root@gmail.com',
+            'root'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
